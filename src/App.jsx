41c5a70a11b472661e3b9fa12432ff97d1bb98b2
khachikgevorgyan <khachgevorgyan94@gmail.com>
2024-10-31 12:50:32 +0400

    project started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// function App(){
+//   return <div>
+//     <h1>Hello</h1>
+//     <span/>
+//   </div>
+// }
+//
+//
+// export default App
+
+export const App = ()=>{
+  return <div>
+    <div></div>
+    <h1>asklfjasklf</h1>
+  </div>
+}
